1c7b474da782f1e065e573f139532d39fe7b7151
Eric Crosson <eric.s.crosson@utexas.edu>
2020-02-18 10:38:18 -0600

    fix: modify boolean test for debug condition

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { positionSizeCalculator } from './position-size-calculator'
+
+function main(): void {
+    positionSizeCalculator(process.argv.slice(2))
+        .bimap(
+            (error) => {
+                console.error(`Error:`, error)
+                process.exit(-1)
+            },
+            console.log.bind(null)
+        )
+}
+
+main()
--- src/position-size-calculator.ts
+++ src/position-size-calculator.ts
@@ -134,19 +134,3 @@ export function positionSizeCalculator(
         }
     )
 }
-
-function main(): void {
-    positionSizeCalculator(process.argv.slice(2))
-        .bimap(
-            (error) => {
-                console.error(`Error:`, error)
-                process.exit(-1)
-            },
-            console.log.bind(null)
-        )
-}
-
-
-if (require.main === module) {
-    main()
-}
